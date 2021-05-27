314bcd88b36a4d7bc232a7761ee0edca75198355
Sergiod3x <sergiod3x.@gmail.com>
2021-05-27 13:08:12 +0200

    inespugnabile completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Non sottovalutare la potenza di PlayStation");
+
+var nameP = prompt("inserisci il tuo nome");
+var surnameP = prompt("inserisci il tuo cognome");
+var colorP = prompt("Inserisci il tuo colore preferito");
+
+var inespugnabile = nameP+surnameP+colorP;
+
+document.getElementById("out").innerHTML="La tua 'inespugnabile' password :" + inespugnabile+21 ;
